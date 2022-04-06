75ee025c6b1aee162de2e7a9f9495a0d9fa4ccb5
Omar Amin <oamin.omar@gmail.com>
2022-04-06 03:40:06 +0200

    edited style of start up

--- src/components/StartupPage/Start.js
+++ src/components/StartupPage/Start.js
@@ -1,76 +1,101 @@
 import React from "react";
 import "./stylez.css";
-import { useNavigate  } from "react-router-dom";
+import background from "./Images/whatshappening.png";
+import { useNavigate } from "react-router-dom";
 const Start = () => {
   let history = useNavigate();
   return (
-    <div className="Startf1">
-      <div className="containerf1">
-        <div className="imagef1"></div>
-        <div className="contentf1">
+    <div className="Start">
+      <div className="upf1">
+        <div className="rightf1">
+          <img className="backf1" src={background} />
+        </div>
+        <div className="leftf1">
           <h1 className="h1f1">Happening now</h1>
           <h3 className="h3f1">Join Sirius today.</h3>
-          <div className="buttonf1">
-            <a href="#" className="btnf1">
-              Sign up with Google
+          <button className="signupgooglef1">Sign up with Google</button>
+          <button className="signupfacebookf1"> Sign up with Facebook </button>
+          <p className="pf1">
+            <span className="spf1">or</span>
+          </p>
+          <a href="#" className="signup">
+            Sign up with phone or email
+          </a>
+          <p className="terms">
+            By signing up, you agree to the
+            <a className="tos" href="#">
+              {" "}
+              Terms of Service{" "}
             </a>
-            <a href="#" className="btnf1 signupfacebook">
-              Sign up with Facebook
+            and{" "}
+            <a className="tos" href="#">
+              {" "}
+              Privacy Policy{" "}
             </a>
-            <p>
-              <span>or</span>
-            </p>
-            <a
-              onClick={() => {
-                history.push("/signup");
-              }}
-              href="#"
-              className="btn-signupf1"
-            >
-              Sign up with phone or email
-            </a>
-            <p className="termsf1">
-              By signing up, you agree to the
-              <a href="#"> Terms of Service </a>
-              and <a href="#"> Privacy Policy </a>, including
-              <a href="#"> Cookie Use </a>.
-            </p>
-            <h2 className="alreadyf1">Already have an account?</h2>
-
-            <a
-              onClick={() => {
-                history.push("./login");
-              }}
-              href="#"
-              className="btn-loginf1"
-            >
-              Sign in
-            </a>
-          </div>
+            , including
+            <a href="#"> Cookie Use </a>.
+          </p>
+          <h2 className="alreadyf1">Already have an account?</h2>
+          <a href="#" className="login">
+            Sign in
+          </a>
         </div>
-
-        <div className="footerf1">
-          <nav>
-            <a className="af1" href="#">About</a>
-            <a className="af1" href="#">help center</a>
-            <a className="af1" href="#">terms of service</a>
-            <a className="af1" href="#">privacy policy</a>
-            <a className="af1" href="#">cookie policy</a>
-            <a className="af1" href="#">Accessibility</a>
-            <a className="af1" href="#">ads info</a>
-            <a className="af1" href="#">blog</a>
-            <a className="af1" href="#">status</a>
-            <a className="af1" href="#">careers</a>
-            <a className="af1" href="#">brand resources</a>
-            <a className="af1" href="#">advertising</a>
-            <a className="af1" href="#">marketing</a>
-            <a className="af1" href="#">twitter for business</a>
-            <a className="af1" href="#">developers</a>
-            <a className="af1" href="#">directory</a>
-            <a className="af1" href="#">settings</a>
-          </nav>
-          <footer className="footerf1">&copy; 2022 Sirius, Inc.</footer>
-        </div>
+      </div>
+      <div className="footerf1">
+        <nav>
+          <a className="a1f1" href="#">
+            About
+          </a>
+          <a className="a1f1" href="#">
+            help center
+          </a>
+          <a className="a1f1" href="#">
+            terms of service
+          </a>
+          <a className="a1f1" href="#">
+            privacy policy
+          </a>
+          <a className="a1f1" href="#">
+            cookie policy
+          </a>
+          <a className="a1f1" href="#">
+            Accessibility
+          </a>
+          <a className="a1f1" href="#">
+            ads info
+          </a>
+          <a className="a1f1" href="#">
+            blog
+          </a>
+          <a className="a1f1" href="#">
+            status
+          </a>
+          <a className="a1f1" href="#">
+            careers
+          </a>
+          <a className="a1f1" href="#">
+            brand resources
+          </a>
+          <a className="a1f1" href="#">
+            advertising
+          </a>
+          <a className="a1f1" href="#">
+            marketing
+          </a>
+          <a className="a1f1" href="#">
+            twitter for business
+          </a>
+          <a className="a1f1" href="#">
+            developers
+          </a>
+          <a className="a1f1" href="#">
+            directory
+          </a>
+          <a className="a1f1" href="#">
+            settings
+          </a>
+        </nav>
+        <footer className="copyf1">&copy; 2022 Sirius, Inc.</footer>
       </div>
     </div>
   );
