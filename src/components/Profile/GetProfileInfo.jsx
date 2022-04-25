8a77faea185381cb8ea58982398713102c6c719f
Omar Amin <oamin.omar@gmail.com>
2022-04-25 21:43:11 +0200

    edit getprofile route
    working for boody only for now

--- src/components/Profile/GetProfileInfo.jsx
+++ src/components/Profile/GetProfileInfo.jsx
@@ -2,13 +2,13 @@ import axios from "axios";
 var token = localStorage.getItem("tokenValue");
 var config = {
   method: 'get',
-  url: 'http://34.236.108.123:3000/Boody',
+  url: 'http://34.236.108.123:3000/boody',
   headers: {Authorization:"Bearer "+token }
 };
 export default async function GetProfileInfo() {
   let response = '';
   try {
-    response = await axios.get('http://34.236.108.123:3000/Boody',config).then((res) => res.data);
+    response = await axios.get('http://34.236.108.123:3000/boody',config).then((res) => res.data);
     return (response);
   } catch (error) {
     if (error.response) {
