fd8becc239d9492603a68d9688b1271b5e537f63
Omar Amin <oamin.omar@gmail.com>
2022-04-27 01:40:45 +0200

    fixed url in posting a tweet

--- src/components/Home/constants/Inputtext.jsx
+++ src/components/Home/constants/Inputtext.jsx
@@ -32,7 +32,7 @@ function Inputtext(props)
         async function PostTweet() {
           let response = '';
           try {
-            response = await axios.post('http://34.236.108.123:3000/compose-tweet',{body:text,media: []},{headers: {Authorization:"Bearer "+token}}).then((res) => res.data);
+            response = await axios.post('http://34.236.108.123:3000/home/compose-tweet',{body:text,media: []},{headers: {Authorization:"Bearer "+token}}).then((res) => res.data);
             
             return (response.data);
           } catch (error) {
